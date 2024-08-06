08e4c82b4a29ba3ff3877cfefd1c128d9751c237
Vacharck <Vacharck@gmail.com>
2024-08-06 14:23:40 -0400

    alarm view created

--- app/views/alarm.view.php
+++ app/views/alarm.view.php
@@ -1,2 +1,19 @@
 <h1>Alarm page</h1>
 
+<div>
+    <form action="post">
+        <label for="title">
+            Title:
+        </label>
+        <input type="text" name="title" id="title">
+        <label for="summary">
+            Summary:
+        </label>
+        <input type="text" name="summary" id="summary">
+        <label for="date">
+            Date:
+        </label>
+        <input type="datetime" name="date" id="date">
+        <input type="submit" value="Create alarm">
+    </form>
+</div>
